5dee3cc668f4a2ce26a5c2d5b153a33fda229682
Austin See <halfblowncontact@gmail.com>
2024-04-09 21:05:06 -0700

    nest api /v1

--- src/api/controller/mod.rs
+++ src/api/controller/mod.rs
@@ -15,9 +15,16 @@ pub use super::{ApiError, Result};
 // endregion: module imports and declarations
 
 pub(super) fn add_routes(router: Router) -> Router {
-    router
+    // Add routes not related to API controllers
+    let router = router
         .route("/healthz", get(health::healthz))
-        .route("/readyz", get(health::readyz))
+        .route("/readyz", get(health::readyz));
+
+    // Create API controller router
+    let api_v1 = Router::new()
         .route("/", get(hello_world::handler))
-        .route("/echo/:name", get(hello_world::echo))
+        .route("/echo/:name", get(hello_world::echo));
+
+    // Nest API controllers under /v1
+    router.nest("/v1", api_v1)
 }
